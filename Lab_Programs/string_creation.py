d8ee59557ed502b1ea48bacb551626988a91d34d
Shampy <157746114+codershampy@users.noreply.github.com>
2024-06-09 11:37:01 +0530

    Add files via upload

---
+++ Lab_Programs/string_creation.py
@@ -0,0 +1,13 @@
+
+#creation
+string="Computer Science"
+print(string)
+#indexing
+print(string[13])
+print(string[3:12])
+#concatenation
+new_string = string+"is a branch"
+print(new_string)
+#repetition
+r_string=string*5
+print(r_string)

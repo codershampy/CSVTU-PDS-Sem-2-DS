baf0d65af5a418445698227f609ecd9e3bfc5dcf
Shampy <157746114+codershampy@users.noreply.github.com>
2024-06-11 20:36:58 +0530

    Add files via upload

---
+++ Lab_Programs/set_creation.py
@@ -0,0 +1,9 @@
+#creation
+set1={1,2,3,4,5}
+set2=(6,7,8,9,10)
+set3=set1.union(set2) #concatenating using '.union'&'1'.
+set4= set1|set2
+print(set1)
+print(set2)#indexing,slicing,repetition not applicable in set.
+print(set3)
+print(set4)

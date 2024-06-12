9c5376b0502392e74f3c678de6a3a3ce9b8429bc
Shampy <157746114+codershampy@users.noreply.github.com>
2024-06-12 22:47:04 +0530

    Add files via upload

---
+++ Lab_Programs/list_creation.py
@@ -0,0 +1,14 @@
+#creation
+list = [2,4,5,6,7,8,9,80]
+print(list)
+#indexing
+print(list[5])
+#slicing
+print(list[3:6])
+print(list[-2: ])
+#concatenation
+new_list = list +[14,23,44]
+print(new_list)
+#repetition
+r_list = list*4
+print (r_list)

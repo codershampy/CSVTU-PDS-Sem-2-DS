a7660ead74779f336cf5a7d43cf56a781525b8bb
Shampy <157746114+codershampy@users.noreply.github.com>
2024-06-13 23:23:20 +0530

    Add files via upload

---
+++ Lab_Programs/tuple.py
@@ -0,0 +1,12 @@
+#creation
+tuple = (12,23,34,45,56,67,78)
+print("Tuple elements are:",tuple)
+#indexing
+print(tuple[6])
+print(tuple[2:7])
+#concatenation
+new_tuple = tuple+(89.90,)
+print(new_tuple)
+#repetition
+r_tuple=tuple*2
+print(r_tuple)
